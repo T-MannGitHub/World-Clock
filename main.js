354539362724fd5e7122f1695564f74225e1cccd
T-MannGitHub <127382999+T-MannGitHub@users.noreply.github.com>
2025-07-14 19:33:33 +0930

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { getDataForCity } from './city.js';
 import { renderClocks } from './clock.js';
 
-const cities = ['London', 'Melbourne', 'Adelaide', 'Darwin'];
+const cities = ['London', 'Melbourne', 'Adelaide', 'Darwin', 'Coolgardie'];
 
 async function updateClocks() {
     const cityTimes = await Promise.all(
